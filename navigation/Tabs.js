eec00626bbb9504217d60a242097da8e0dcee03f
Seonwoo <wlstjsdn12@gmail.com>
2023-05-22 10:39:02 +0900

    .

--- navigation/Tabs.js
+++ navigation/Tabs.js
@@ -14,6 +14,9 @@ const Tabs = () => {
   const isDark = useColorScheme() === "dark";
   return (
     <Tab.Navigator
+      sceneContainerStyle={{
+        backgroundColor: isDark ? BLACK_COLOR : "white",
+      }}
       screenOptions={{
         tabBarStyle: {
           backgroundColor: isDark ? BLACK_COLOR : "white",
